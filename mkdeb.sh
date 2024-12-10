409c7ab1379d36e6db650f6258688d54bd5fcc62
gongyong <185457686@qq.com>
2024-12-10 16:26:47 +0800

    replace coping gbinder.so with installing gbinder-python.tar.gz; add depends for compileing pyc; optimize control filename

--- mkdeb.sh
+++ mkdeb.sh
@@ -7,15 +7,8 @@ if [ -z "$ver" ];then
 	exit 1
 fi
 
-if [ ! -e ~/tigervnc-standalone-server_1.10.1+dfsg-3_arm64.deb ];then
-	echo "Error: ~/gitervnc-standalone-server_1.10.1+dfsg-3_arm64.deb not exist. please build the project fde_tigervncserver"
-	exit 1
-fi
-
-sudo find /usr -name "gbinder.cpython*aarch64-linux-gnu.so" > /tmp/gbinder.list
-n=`cat /tmp/gbinder.list |wc -l`
-if  [  $n = 0  ] ;then
-	echo "Error: cant't find gbinder.cpython*aarch64-linux-gnu.so "
+if [ ! -e ~/gbinder-python/dist/gbinder-python-1.0.0.tar.gz  ];then
+	echo "Error: ~/gbinder-python/dist/gbinder-python-1.0.0.tar.gz not exist. please build the project gbinder-python with python3 setup.py sdist --cython "
 	exit 1
 fi
 
@@ -28,7 +21,6 @@ dst_dir=`ls debian/ -l |grep ^d |awk -F " " '{print $NF}' |tr -d " "`
 
 dst=debian/$dst_dir
 sudo rm -rf list/waydroidlist
-sudo find /usr -name "gbinder.cpython*aarch64-linux-gnu.so" >> list/waydroidlist
 source /etc/lsb-release
 cat list/waydroid.list |sudo tee -a list/waydroidlist 1>/dev/null
 cat list/mutter.list |sudo tee -a list/waydroidlist 1>/dev/null
@@ -38,7 +30,7 @@ if [ "$DISTRIB_ID" = "Kylin" ];then
 	cat list/kylinfde.list |sudo tee -a list/waydroidlist  1>/dev/null
 	cp -a debian/control.kylinv10sp1 ${dst}/debian/control
 elif [ "$DISTRIB_ID" = "Ubuntu" ];then
-	cp -a debian/control.ubuntu22.04 ${dst}/debian/control
+	cp -a debian/control.ubuntu_$DISTRIB_CODENAME ${dst}/debian/control
 elif [ "$DISTRIB_ID" == "uos" ] ;then
 	cat list/weston.list |sudo tee -a list/waydroidlist 1>/dev/null
 	cp -a debian/control.uos20eagle ${dst}/debian/control
@@ -68,13 +60,10 @@ if [ "$choice" = "y" ];then
 fi
 
 #step 3 make src.xz
-sudo rm -rf /tmp/tigervnc-standalone
-mkdir /tmp/tigervnc-standalone
-sudo dpkg-deb -x ~/tigervnc-standalone-server_1.10.1+dfsg-3_arm64.deb /tmp/tigervnc-standalone
-sudo tar -cvpf $dst/tigervnc.tar.gz -C /tmp/tigervnc-standalone usr
+sudo cp ~/gbinder-python/dist/gbinder-python-1.0.0.tar.gz $dst/
 
-echo "tar -cJvpf debian/openfde_${ver}.orig.tar.xz  -C $dst fde.tar  tigervnc.tar.gz waydroid_image.tar  waydroid.tar"
-tar -cJvpf debian/openfde_${ver}.orig.tar.xz  -C $dst fde.tar  tigervnc.tar.gz waydroid_image.tar  waydroid.tar
+echo "tar -cJvpf debian/openfde_${ver}.orig.tar.xz  -C $dst fde.tar  waydroid_image.tar  waydroid.tar gbinder-python-1.0.0.tar.gz "
+tar -cJvpf debian/openfde_${ver}.orig.tar.xz  -C $dst fde.tar  waydroid_image.tar  waydroid.tar gbinder-python-1.0.0.tar.gz
 pushd $dst
 #step 4 fill changes
 dch -i 
