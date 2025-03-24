02da3e2032644ea9c21a728298f102be54ccfc86
gongyong <185457686@qq.com>
2025-03-24 16:39:19 +0800

    support arm64only

--- mkdeb.sh
+++ mkdeb.sh
@@ -6,6 +6,10 @@ if [ -z "$ver" ];then
 	echo "Error: please input a version "
 	exit 1
 fi
+if [ $# -eq  2 ];then
+	arm64_only=1
+	echo "arm64only mode"
+fi
 
 if [ ! -e ~/gbinder-python/dist/gbinder-python-1.0.0.tar.gz  ];then
 	echo "Error: ~/gbinder-python/dist/gbinder-python-1.0.0.tar.gz not exist. please build the project gbinder-python with python3 setup.py sdist --cython "
@@ -20,6 +24,31 @@ fi
 dst_dir=`ls debian/ -l |grep ^d |awk -F " " '{print $NF}' |tr -d " "`
 
 dst=debian/$dst_dir
+secs=`echo $dst |awk -F "-" '{print NF}'`
+cur_ver=`echo $dst |awk -F "-" '{print $2}'`
+if [ $arm64_only -eq 1 ];then
+	if [ $secs -eq 2 ];then #2 means normal node
+		base_package=`echo $dst |awk -F "-" '{print $1}'`
+		echo "mv $dst/debian/openfde to $dst/debian/openfde-arm64"
+		sudo mv $dst/debian/openfde $dst/debian/openfde-arm64
+		echo "mv $dst to debian/openfde-arm64-$cur_ver"
+		sudo mv $dst debian/openfde-arm64-$cur_ver
+		dst="debian/openfde-arm64-$cur_ver"
+	elif [ $secs -eq 3 ];then # 3 means already arm64only mode (openfde-arm64-x.x.x)
+		echo "already arm64 only"
+		if [ -e $dst/debian/openfde ];then
+			echo "mv $dst/debian/openfde to $dst/debian/openfde-arm64"
+			sudo mv $dst/debian/openfde $dst/debian/openfde-arm64
+		fi
+	fi
+else
+	if [ $secs -eq 3 ];then #3 means already arm64only mode(openfde-arm64-x.x.x)
+		echo "mv $dst to debian/openfde-$cur_ver"
+		sudo mv $dst debian/openfde-$cur_ver
+		dst="debian/openfde-$cur_ver"
+	fi
+fi
+
 sudo rm -rf list/waydroidlist
 if [ ! -e "/etc/lsb-release" ];then
 	uname -a |grep Debian 1>/dev/null 2>&1
@@ -31,9 +60,7 @@ else
 	source /etc/lsb-release
 fi
 cat list/waydroid.list |sudo tee -a list/waydroidlist 1>/dev/null
-if [ "$DISTRIB_ID" != "Deepin" ] ;then
-	cat list/mutter.list |sudo tee -a list/waydroidlist 1>/dev/null
-fi
+cat list/mutter.list |sudo tee -a list/waydroidlist 1>/dev/null
 if [ "$DISTRIB_ID" = "Kylin" ];then
 	cat list/weston.list |sudo tee -a list/waydroidlist 1>/dev/null
 	cat list/kylinmutter.list |sudo tee -a list/waydroidlist  1>/dev/null
@@ -46,18 +73,29 @@ elif [ "$DISTRIB_ID" = "Debian" ];then
 	cp -a debian/control.debian_$VERSION_CODENAME ${dst}/debian/control
 elif [ "$DISTRIB_ID" = "Ubuntu" ];then
 	cp -a debian/control.ubuntu_$DISTRIB_CODENAME ${dst}/debian/control
-	machine=`uname -m`
-	if [ "$machine" = "x86_64" ];then
-		sed -i "s/arm64/amd64/"  ${dst}/debian/control
-	fi
 elif [ "$DISTRIB_ID" == "uos" ] ;then
 	cat list/weston.list |sudo tee -a list/waydroidlist 1>/dev/null
 	cp -a debian/control.uos20eagle ${dst}/debian/control
-elif [ "$DISTRIB_ID" == "Deepin" ] ;then
-	cat list/weston12.list |sudo tee -a list/waydroidlist 1>/dev/null
-	cp -a debian/control.deepin_$DISTRIB_CODENAME ${dst}/debian/control
 fi
 
+if [ $arm64_only -eq 1 ];then
+	source=`sed -n "/Source/s/.*://p" ${dst}/debian/control |tr -d " " `
+	if [ "$source" = "openfde" ];then
+		sed -i "/Source/s/openfde/openfde-arm64/" ${dst}/debian/control
+	fi
+	package=`sed -n "/Package/s/.*://p" ${dst}/debian/control |tr -d " " `
+	if [ "$package" = "openfde" ];then
+		sed -i "/Package/s/openfde/openfde-arm64/" ${dst}/debian/control
+	fi
+	change=`sed -n "1s/(.*//p" ${dst}/debian/changelog |tr -d " "`
+	if [ "$change" = "openfde" ];then
+		sed -i "1s/openfde/openfde-arm64/" ${dst}/debian/changelog
+	fi
+else
+	sed -i "/Source/s/openfde-arm64/openfde/" ${dst}/debian/control
+	sed -i "/Package/s/openfde-arm64/openfde/" ${dst}/debian/control
+	sed -i "1s/openfde-arm64/openfde/" ${dst}/debian/changelog
+fi
 
 #step 1 tar fde
 echo "Step 1: will tar file from the below list"
@@ -84,8 +122,13 @@ fi
 #step 3 make src.xz
 sudo cp ~/gbinder-python/dist/gbinder-python-1.0.0.tar.gz $dst/
 
-echo "tar -cJvpf debian/openfde_${ver}.orig.tar.xz  -C $dst fde.tar  waydroid_image.tar  waydroid.tar gbinder-python-1.0.0.tar.gz "
-tar -cJvpf debian/openfde_${ver}.orig.tar.xz  -C $dst fde.tar  waydroid_image.tar  waydroid.tar gbinder-python-1.0.0.tar.gz
+if [ $arm64_only -eq 1 ];then
+	echo "tar -cJvpf debian/openfde-arm64_${ver}.orig.tar.xz  -C $dst fde.tar  waydroid_image.tar  waydroid.tar gbinder-python-1.0.0.tar.gz "
+	#tar -cJvpf debian/openfde-arm64_${ver}.orig.tar.xz  -C $dst fde.tar  waydroid_image.tar  waydroid.tar gbinder-python-1.0.0.tar.gz
+else
+	echo "tar -cJvpf debian/openfde_${ver}.orig.tar.xz  -C $dst fde.tar  waydroid_image.tar  waydroid.tar gbinder-python-1.0.0.tar.gz "
+	tar -cJvpf debian/openfde_${ver}.orig.tar.xz  -C $dst fde.tar  waydroid_image.tar  waydroid.tar gbinder-python-1.0.0.tar.gz
+fi
 pushd $dst
 #step 4 fill changes
 if [ ! -e /usr/bin/dch ];then
