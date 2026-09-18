0cd02872180e9cc28a61538d706b25af86921cb3
gongyong <185457686@qq.com>
2026-09-18 14:32:49 +0800

    remove unsed code

--- packapk.sh
+++ packapk.sh
@@ -21,10 +21,8 @@ if [ $? != 0 ];then
 	echo "Error: copy system failed"
 	exit 1
 fi
-#sudo rm -rf  system/system/product/app/LatinIME/
 sudo rm -rf system/system/media/bootanimation.zip
 sudo mkdir system/system/product/app/iflytek
-#sudo mkdir system/volumes
 sudo cp -a iflytek.apk system/system/product/app/iflytek
 if [ $? != 0 ];then
 	echo "Error: copy iflytek failed"
